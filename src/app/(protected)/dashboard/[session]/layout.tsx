cc686fe2cc8d9e5f48ff99a03af3d11594089001
laurhakob <laurhakob@gmail.com>
2026-03-16 22:49:44 +0400

    dashboard and navbar

---
+++ src/app/(protected)/dashboard/[session]/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/navbar";
+import { SubscriptionEntitlementQuery } from "@/convex/query.config";
+import { combinedSlug } from "@/lib/utils";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = async ({ children }: Props) => {
+  const { profileName, entitlement } = await SubscriptionEntitlementQuery();
+
+  if (!entitlement._valueJSON) {
+    // TODO: Remove billing hardcoded path
+  //  redirect(`/dashboard/${combinedSlug(profileName!)}`);
+  }
+
+  return (
+    <div className="grid grid-cols-1">
+      <Navbar /> 
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
