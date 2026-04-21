59b3b27b7e876580e149376a1827f7cbf37d8789
laurhakob <laurhakob@gmail.com>
2026-04-21 21:02:00 +0400

    add convex prod

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,34 @@
+// import { convexAuth } from "@convex-dev/auth/server";
+// import Google from "@auth/core/providers/google";
+// import { Password } from "@convex-dev/auth/providers/Password";
+
+// export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+//   providers: [Google, Password],
+// });
+
+// import { convexAuth } from "@convex-dev/auth/server";
+// import Google from "@auth/core/providers/google";
+// import { Password } from "@convex-dev/auth/providers/Password";
+
+// export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+//   providers: [
+//     Google({
+//       clientId: process.env.AUTH_GOOGLE_ID!,
+//       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+//     }),
+//     Password,
+//   ],
+// });
+
+
+
 import { convexAuth } from "@convex-dev/auth/server";
 import Google from "@auth/core/providers/google";
 import { Password } from "@convex-dev/auth/providers/Password";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Google, Password],
+  providers: [
+    Google,
+    Password,
+  ],
 });
