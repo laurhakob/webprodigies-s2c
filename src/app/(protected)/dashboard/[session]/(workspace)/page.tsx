3a7836da5af57760c86cf0ea286da88e7a454144
laurhakob <laurhakob@gmail.com>
2026-04-22 10:09:53 +0400

    vercel check

--- src/app/(protected)/dashboard/[session]/(workspace)/page.tsx
+++ src/app/(protected)/dashboard/[session]/(workspace)/page.tsx
@@ -1,3 +1,29 @@
+// import ProjectsList from "@/components/projects/list";
+// import ProjectsProvider from "@/components/projects/list/provider";
+// import { ProjectsQuery } from "@/convex/query.config";
+// import { redirect } from "next/navigation";
+// import React from "react";
+
+// const Page = async () => {
+//   const { projects, profile } = await ProjectsQuery();
+
+//   if (!profile) {
+//     redirect("/auth/sign-in");
+//   }
+
+//   return (
+//     <ProjectsProvider initialProjects={projects}>
+//       <div className="container mx-auto py-36 px-4">
+//         <ProjectsList />
+//       </div>
+//     </ProjectsProvider>
+//   );
+// };
+
+// export default Page;
+
+
+
 import ProjectsList from "@/components/projects/list";
 import ProjectsProvider from "@/components/projects/list/provider";
 import { ProjectsQuery } from "@/convex/query.config";
@@ -6,6 +32,8 @@ import React from "react";
 
 const Page = async () => {
   const { projects, profile } = await ProjectsQuery();
+
+  console.log("DEBUG profile:", JSON.stringify(profile));
 
   if (!profile) {
     redirect("/auth/sign-in");
